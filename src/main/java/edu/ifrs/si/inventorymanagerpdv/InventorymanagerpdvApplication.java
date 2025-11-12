3bb18ca7351bda58fbda8ef7776a5bc95f4ed186
Lucas Rech <lucasrech00@gmail.com>
2025-11-11 21:37:21 -0300

    First Commit

---
+++ src/main/java/edu/ifrs/si/inventorymanagerpdv/InventorymanagerpdvApplication.java
@@ -0,0 +1,13 @@
+package edu.ifrs.si.inventorymanagerpdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymanagerpdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymanagerpdvApplication.class, args);
+	}
+
+}
