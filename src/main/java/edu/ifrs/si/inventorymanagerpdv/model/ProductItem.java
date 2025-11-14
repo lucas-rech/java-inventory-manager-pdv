903a63abc6dba97a2b1cc78a5153d2957363c5ae
Lucas Rech <lucasrech00@gmail.com>
2025-11-14 00:37:45 -0300

    refactoring test database tablenames

--- src/main/java/edu/ifrs/si/inventorymanagerpdv/model/ProductItem.java
+++ src/main/java/edu/ifrs/si/inventorymanagerpdv/model/ProductItem.java
@@ -3,7 +3,9 @@ package edu.ifrs.si.inventorymanagerpdv.model;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table(name = "product_item")
 public record ProductItem(
         @Id Long id,
         String name,
