7bcfa70c92929a5b0a83fd7c8ed237e36e47ccc0
Lucas Rech <lucasrech00@gmail.com>
2025-11-20 10:58:35 -0300

    adding deserialization test

--- src/test/java/edu/ifrs/si/inventorymanagerpdv/consumer/ConsumerJsonTest.java
+++ src/test/java/edu/ifrs/si/inventorymanagerpdv/consumer/ConsumerJsonTest.java
@@ -49,7 +49,44 @@ public class ConsumerJsonTest {
         assertThat(json.write(consumer)).extractingJsonPathStringValue("@.createdAt").isEqualTo("2024-05-20T14:30:00");
         assertThat(json.write(consumer)).hasJsonPathStringValue("@.updatedAt");
         assertThat(json.write(consumer)).extractingJsonPathStringValue("@.updatedAt").isEqualTo("2024-05-20T14:30:00");
-
     }
 
+
+
+    @Test
+    void consumerDeserializeTest() throws Exception {
+        String expected = """
+                {
+                      "id": 40,
+                      "name": "Alex",
+                      "lastName": "Turner",
+                      "email": "alexturner@gmail.com",
+                      "document": "94387634945",
+                      "phoneNumber": "54 999998888",
+                      "createdAt": "2024-05-20T14:30:00",
+                      "updatedAt": "2024-05-20T14:30:00"
+                }
+                """;
+
+        assertThat(json.parse(expected)).isEqualTo(
+                new Consumer(
+                        40L,
+                        "Alex",
+                        "Turner",
+                        "alexturner@gmail.com",
+                        "94387634945",
+                        "54 999998888",
+                        LocalDateTime.parse("2024-05-20T14:30:00"),
+                        LocalDateTime.parse("2024-05-20T14:30:00")
+                ));
+
+        assertThat(json.parseObject(expected).id()).isEqualTo(40);
+        assertThat(json.parseObject(expected).name()).isEqualTo("Alex");
+        assertThat(json.parseObject(expected).lastName()).isEqualTo("Turner");
+        assertThat(json.parseObject(expected).email()).isEqualTo("alexturner@gmail.com");
+        assertThat(json.parseObject(expected).document()).isEqualTo("94387634945");
+        assertThat(json.parseObject(expected).phoneNumber()).isEqualTo("54 999998888");
+        assertThat(json.parseObject(expected).createdAt()).isEqualTo(LocalDateTime.parse("2024-05-20T14:30:00"));
+        assertThat(json.parseObject(expected).updatedAt()).isEqualTo(LocalDateTime.parse("2024-05-20T14:30:00"));
+    }
 }
